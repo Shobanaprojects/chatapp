393d6882debfb92ff26e826cdefadfa64efaeadd
Shobana <shobzzz125@gmail.com>
2023-11-10 10:00:34 +0530

    second commit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -32,7 +32,7 @@ const Chats = () => {
 
     return(
         <div className="chats">
-            {chats?.sort((a,b) => b[1].date - a[1].date).map((chat) => (
+            {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
                 <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
                     <img src={chat[1].userInfo.photoURL} alt=""/>
                     <div className="userChatinfo">
